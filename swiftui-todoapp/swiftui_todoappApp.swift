06cd3797c11c8b2308c78b0589316af0bbb54b9b
Bryan Pinheiro <bryansouza_01@outlook.com>
2023-10-03 21:56:20 -0300

    Initial Commit

---
+++ swiftui-todoapp/swiftui_todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_todoappApp.swift
+//  swiftui-todoapp
+//
+//  Created by Bryan on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_todoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
